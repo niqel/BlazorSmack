de0af5378415c49bd52334281b1a89b50e5add46
Gustavo Melendez <niqel504@hotmail.com>
2023-09-08 01:24:11 -0600

    Add BalorSmackComponent

---
+++ BlazorSmackGame/Pages/BlazorSmackComponent.cs
@@ -0,0 +1,12 @@
+namespace BlazorSmackGame.Pages
+{
+    public partial class BlazorSmackComponent
+    {
+        int score = 0;
+        int currentTime = 10;
+        int hitPosition = 0;
+        string message = "";
+        int gameSpeed = 10;
+
+    }
+}
